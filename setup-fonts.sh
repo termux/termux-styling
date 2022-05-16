237603e02a9b3d505dd332d4d9d3738d7a604140
KN4CK3R <admin@oldschoolhack.me>
2022-05-16 12:38:29 +0200

    Fix dpkg-query in setup-fonts.sh

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -11,7 +11,7 @@ rm -Rf $TMPDIR
 mkdir -p $TMPDIR
 
 # Install python fontforge bindings for use by powerline font patcher:
-if [ $(dpkg-query -W -f='${Status}' python-fontforge 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+if [ $(dpkg-query -W -f='${Status}' python3-fontforge 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
 	sudo apt install python3-fontforge
 fi
 
